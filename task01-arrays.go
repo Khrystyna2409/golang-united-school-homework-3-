d894dc18768a2d854dd4b02447c364a753591fe7
Khrystyna2409 <xrustuna.di@gmail.com>
2022-05-07 19:58:36 +0300

    files

--- task01-arrays.go
+++ task01-arrays.go
@@ -1,6 +1,18 @@
 package homework
+import fmt
+
+import "test/go/pkg/mod/golang.org/x/mod@v0.3.0/sumdb"
 
 func average(input [15]float32) (result float32) {
 	//Place your code here
+	a := [15]float32{1, 2, 3, 4, 5, 6}
+	var sum float32
+	for i :=0; i<5; i++{
+		sum += a[i]
+	}
+	lenmy := len(a)
+	fmt.Println (sum / lenmy)
+
+	//not change
 	return
 }
